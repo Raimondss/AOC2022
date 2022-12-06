2c172237e8e85adac6d7a61a5157da26f9b1bc5e
Raimonds Purvinskis <raimonds.purvinskis@printful.com>
2022-12-06 19:18:55 +0200

    second task

--- Processors/Day6.go
+++ Processors/Day6.go
@@ -17,36 +17,46 @@ func (Day6 Day6) FirstTaskResult() {
 
 	characters := strings.Split(inputString, "")
 
+	result := getFirstUniqueOccurrence(characters, 4)
+	fmt.Println(result)
+}
+
+func getFirstUniqueOccurrence(characters []string, length int) int {
 	var result int
-	for i := 0; i < len(characters)-4; i++ {
-		chars := []string{characters[i], characters[i+1], characters[i+2], characters[i+3]}
+	for i := 0; i < len(characters)-length; i++ {
+		var chars []string
+		for y := 0; y < length; y++ {
+			chars = append(chars, characters[y+i])
+		}
 
 		if !containsDuplicate(chars) {
-			result = i + 4
+			result = i + length
 			break
 		}
 	}
 
-	fmt.Println(result)
+	return result
 }
 
 func containsDuplicate(stringArray []string) bool {
 	valueMap := make(map[string]string)
 	for _, value := range stringArray {
 		if valueMap[value] != "" {
-			fmt.Println("duplicate:")
-			fmt.Println(stringArray)
 			return true
 		} else {
 			valueMap[value] = value
 		}
 	}
 
-	fmt.Println("No duplicate:")
-	fmt.Println(stringArray)
 	return false
 }
 
 func (Day6 Day6) SecondTaskResult() {
-	fmt.Println("Second task:")
+	fmt.Println("First task:")
+	inputString := FileReader.GetString(Day6.InputFilePath)
+
+	characters := strings.Split(inputString, "")
+
+	result := getFirstUniqueOccurrence(characters, 14)
+	fmt.Println(result)
 }
