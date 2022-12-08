4349ff44251bd00acc54358c599226b53dbcbb1a
Raimonds Purvinskis <raimonds.purvinskis@printful.com>
2022-12-08 23:50:43 +0200

    first task

--- Processors/Day8.go
+++ Processors/Day8.go
@@ -34,113 +34,114 @@ func getTreeMap(Day Day8) (map[string]int, int, int) {
 func (Day Day8) FirstTaskResult() {
 	treeMap, rows, columns := getTreeMap(Day)
 
-	isVisibleLeft(1, 1, treeMap, columns, rows)
-
 	visibleTrees := 0
+	bestScenicScore := 0
 	for y := 0; y < rows; y++ {
 		for x := 0; x < columns; x++ {
-			if isTreeVisible(x, y, treeMap, columns, rows) {
+			isVisible, scenicScore := isTreeVisible(x, y, treeMap, columns, rows)
+
+			if scenicScore > bestScenicScore {
+				bestScenicScore = scenicScore
+			}
+
+			if isVisible {
 				visibleTrees++
 			}
 		}
 	}
 
 	fmt.Println(visibleTrees)
+	fmt.Println(bestScenicScore)
 }
 
-func isTreeVisible(treeXCord int, treeYCord int, treeMap map[string]int, columns int, rows int) bool {
-	if isVisibleUp(treeXCord, treeYCord, treeMap, columns, rows) {
-		println("IS VISIBLE UP " + getCoordKey(treeXCord, treeYCord))
-		return true
-	}
+func isTreeVisible(treeXCord int, treeYCord int, treeMap map[string]int, columns int, rows int) (bool, int) {
 
-	if isVisibleDown(treeXCord, treeYCord, treeMap, columns, rows) {
-		println("IS VISIBLE DOWN " + getCoordKey(treeXCord, treeYCord))
-		return true
-	}
+	isVisibleUp, visibilityUpLenght := isVisibleUp(treeXCord, treeYCord, treeMap, columns, rows)
+	isVisibleDown, visiblityDownLenght := isVisibleDown(treeXCord, treeYCord, treeMap, columns, rows)
+	isVisibleRight, visiblityRightLenght := isVisibleRight(treeXCord, treeYCord, treeMap, columns, rows)
+	isVisibleLeft, visibilityLeftLeght := isVisibleLeft(treeXCord, treeYCord, treeMap, columns, rows)
 
-	if isVisibleRight(treeXCord, treeYCord, treeMap, columns, rows) {
-		println("IS VISIBLE RIGHT " + getCoordKey(treeXCord, treeYCord))
-		return true
-	}
+	isVisible := isVisibleUp || isVisibleDown || isVisibleLeft || isVisibleRight
+	scenicScore := visibilityUpLenght * visiblityDownLenght * visibilityLeftLeght * visiblityRightLenght
 
-	if isVisibleLeft(treeXCord, treeYCord, treeMap, columns, rows) {
-		println("IS VISIBLE LEFT " + getCoordKey(treeXCord, treeYCord))
-		return true
-	}
-
-	println("IS NOT VISIBLE " + getCoordKey(treeXCord, treeYCord))
-	return false
+	return isVisible, scenicScore
 }
 
-func isVisibleDown(x int, y int, treeMap map[string]int, columns int, rows int) bool {
+func isVisibleDown(x int, y int, treeMap map[string]int, columns int, rows int) (bool, int) {
 	currentTreeSize := treeMap[getCoordKey(x, y)]
 	//currentTreeCordKey := getCoordKey(x, y)
-
+	visibilityLenght := 0
 	for y := y + 1; y < columns; y++ {
 		treeToCheckSize := treeMap[getCoordKey(x, y)]
+		visibilityLenght++
 
 		if treeToCheckSize >= currentTreeSize {
 			//fmt.Println(currentTreeCordKey + " :DOWN _LARGER/SAME FOUND at:" + getCoordKey(x, y))
-			return false
+			return false, visibilityLenght
 		}
 	}
 
 	//fmt.Println(currentTreeCordKey + ":DOWN NOT FOUND")
-	return true
+	return true, visibilityLenght
 }
 
-func isVisibleUp(x int, y int, treeMap map[string]int, columns int, rows int) bool {
+func isVisibleUp(x int, y int, treeMap map[string]int, columns int, rows int) (bool, int) {
 	currentTreeSize := treeMap[getCoordKey(x, y)]
 	//currentTreeCordKey := getCoordKey(x, y)
 
+	visibilityLenght := 0
 	for y := y + -1; y >= 0; y-- {
 		treeToCheckSize := treeMap[getCoordKey(x, y)]
-
+		visibilityLenght++
 		if treeToCheckSize >= currentTreeSize {
 			//fmt.Println(currentTreeCordKey + ":UP :LARGER/SAME FOUND at:" + getCoordKey(x, y))
-			return false
+			return false, visibilityLenght
 		}
 	}
 
 	//fmt.Println(currentTreeCordKey + "UP Larger NOT FOUND")
-	return true
+	return true, visibilityLenght
 }
 
-func isVisibleRight(x int, y int, treeMap map[string]int, columns int, rows int) bool {
+func isVisibleRight(x int, y int, treeMap map[string]int, columns int, rows int) (bool, int) {
 	currentTreeSize := treeMap[getCoordKey(x, y)]
 	//currentTreeCordKey := getCoordKey(x, y)
 
+	visibilityLenght := 0
+
 	for x := x + 1; x < rows; x++ {
 		treeToCheckSize := treeMap[getCoordKey(x, y)]
+		visibilityLenght++
 
 		if treeToCheckSize >= currentTreeSize {
 			//fmt.Println(currentTreeCordKey + ": -->>RIGHT LARGER/SAME FOUND at:" + getCoordKey(x, y))
-			return false
+			return false, visibilityLenght
 		}
 	}
 
 	//fmt.Println(currentTreeCordKey + " :Larger NOT FOUND")
-	return true
+	return true, visibilityLenght
 }
 
-func isVisibleLeft(x int, y int, treeMap map[string]int, columns int, rows int) bool {
+func isVisibleLeft(x int, y int, treeMap map[string]int, columns int, rows int) (bool, int) {
 	currentTreeSize := treeMap[getCoordKey(x, y)]
 	//currentTreeCordKey := getCoordKey(x, y)
 
 	//fmt.Println("___DOWN___:" + currentTreeCordKey)
 
+	visibilityLenght := 0
+
 	for x := x - 1; x >= 0; x-- {
 		treeToCheckSize := treeMap[getCoordKey(x, y)]
-
+		visibilityLenght++
 		if treeToCheckSize >= currentTreeSize {
 			//fmt.Println("LARGER/SAME FOUND at:" + getCoordKey(x, y))
-			return false
+			return false, visibilityLenght
 		}
 	}
 
 	//fmt.Println("Larger NOT FOUND")
-	return true
+	return true, visibilityLenght
 }
 
 func getXY(string string) (int, int) {
