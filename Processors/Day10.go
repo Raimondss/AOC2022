8f7f955bf896be6d26b46257a1ae15ea02356dd0
Raimonds Purvinskis <raimonds.purvinskis@printful.com>
2022-12-10 20:12:49 +0200

    first task

---
+++ Processors/Day10.go
@@ -0,0 +1,128 @@
+package Processors
+
+import (
+	"AOC_2022/FileReader"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Day10 struct {
+	Processor
+	InputFilePath string
+}
+
+func getInput(day10 Day10) []string {
+	return FileReader.GetStringArray(day10.InputFilePath)
+}
+
+func (Day10 Day10) FirstTaskResult() {
+
+	fmt.Println("First:")
+	inputStrings := FileReader.GetStringArray(Day10.InputFilePath)
+	var x = 1
+	var cycle = 1
+
+	totalSignalStrenth := 0
+	for _, inputString := range inputStrings {
+		printCycle(x, cycle)
+		fmt.Println(inputString)
+
+		command := strings.Split(inputString, " ")
+		cyclesToRun := 0
+		valueToAdd := 0
+
+		if cycle == 20 || cycle == 60 || cycle == 100 || cycle == 140 || cycle == 180 || cycle == 220 {
+			signalStrength := cycle * x
+			fmt.Println("Signal strenght:" + strconv.Itoa(signalStrength))
+			totalSignalStrenth += signalStrength
+			fmt.Println("SINGAL STRENGHT UPDATE:" + strconv.Itoa(cycle) + " " + strconv.Itoa(totalSignalStrenth))
+		}
+
+		if command[0] == "addx" {
+			valueToAdd, _ = strconv.Atoi(command[1])
+			cyclesToRun = 1
+		}
+
+		for i := 0; i < cyclesToRun; i++ {
+			cycle++
+
+			if cycle == 20 || cycle == 60 || cycle == 100 || cycle == 140 || cycle == 180 || cycle == 220 {
+				signalStrength := cycle * x
+
+				totalSignalStrenth += signalStrength
+				fmt.Println("Signal strenght:" + strconv.Itoa(signalStrength))
+				fmt.Println("SINGAL STRENGHT UPDATE:" + strconv.Itoa(cycle) + " " + strconv.Itoa(totalSignalStrenth))
+			}
+
+			printCycle(x, cycle)
+		}
+
+		cycle++
+
+		println("Adding value:" + strconv.Itoa(valueToAdd))
+		x += valueToAdd
+	}
+
+	fmt.Println("TOTAL:" + strconv.Itoa(totalSignalStrenth))
+}
+
+func printLine(x) {
+
+}
+
+func printCycle(x int, cycle int) {
+	fmt.Println("CYCLE " + strconv.Itoa(cycle) + " X:" + strconv.Itoa(x))
+}
+
+func (Day10 Day10) SecondTaskResult() {
+	fmt.Println("Second:")
+
+	fmt.Println("First:")
+	inputStrings := FileReader.GetStringArray(Day10.InputFilePath)
+	var x = 1
+	var cycle = 1
+
+	totalSignalStrenth := 0
+	for _, inputString := range inputStrings {
+		printCycle(x, cycle)
+		fmt.Println(inputString)
+
+		command := strings.Split(inputString, " ")
+		cyclesToRun := 0
+		valueToAdd := 0
+
+		if cycle == 20 || cycle == 60 || cycle == 100 || cycle == 140 || cycle == 180 || cycle == 220 {
+			signalStrength := cycle * x
+			fmt.Println("Signal strenght:" + strconv.Itoa(signalStrength))
+			totalSignalStrenth += signalStrength
+			fmt.Println("SINGAL STRENGHT UPDATE:" + strconv.Itoa(cycle) + " " + strconv.Itoa(totalSignalStrenth))
+		}
+
+		if command[0] == "addx" {
+			valueToAdd, _ = strconv.Atoi(command[1])
+			cyclesToRun = 1
+		}
+
+		for i := 0; i < cyclesToRun; i++ {
+			cycle++
+
+			if cycle == 20 || cycle == 60 || cycle == 100 || cycle == 140 || cycle == 180 || cycle == 220 {
+				signalStrength := cycle * x
+
+				totalSignalStrenth += signalStrength
+				fmt.Println("Signal strenght:" + strconv.Itoa(signalStrength))
+				fmt.Println("SINGAL STRENGHT UPDATE:" + strconv.Itoa(cycle) + " " + strconv.Itoa(totalSignalStrenth))
+			}
+
+			printCycle(x, cycle)
+		}
+
+		cycle++
+
+		println("Adding value:" + strconv.Itoa(valueToAdd))
+		x += valueToAdd
+	}
+
+	fmt.Println("TOTAL:" + strconv.Itoa(totalSignalStrenth))
+}
--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 const FilesFolder = "InputFiles"
 
 // DAY TODO User input
-const DAY = 9
+const DAY = 10
 
 func main() {
 	getDayProcessor(DAY).FirstTaskResult()
@@ -21,15 +21,16 @@ func getDayProcessor(dayNumber int) Processors.Processor {
 
 func getAllProcessors() map[int]Processors.Processor {
 	return map[int]Processors.Processor{
-		1: Processors.Day1{InputFilePath: getDayInputFile(1)},
-		2: Processors.Day2{InputFilePath: getDayInputFile(2)},
-		3: Processors.Day3{InputFilePath: getDayInputFile(3)},
-		4: Processors.Day4{InputFilePath: getDayInputFile(4)},
-		5: Processors.Day5{InputFilePath: getDayInputFile(5)},
-		6: Processors.Day6{InputFilePath: getDayInputFile(6)},
-		7: Processors.Day7{InputFilePath: getDayInputFile(7)},
-		8: Processors.Day8{InputFilePath: getDayInputFile(8)},
-		9: Processors.Day9{InputFilePath: getDayInputFile(9)},
+		1:  Processors.Day1{InputFilePath: getDayInputFile(1)},
+		2:  Processors.Day2{InputFilePath: getDayInputFile(2)},
+		3:  Processors.Day3{InputFilePath: getDayInputFile(3)},
+		4:  Processors.Day4{InputFilePath: getDayInputFile(4)},
+		5:  Processors.Day5{InputFilePath: getDayInputFile(5)},
+		6:  Processors.Day6{InputFilePath: getDayInputFile(6)},
+		7:  Processors.Day7{InputFilePath: getDayInputFile(7)},
+		8:  Processors.Day8{InputFilePath: getDayInputFile(8)},
+		9:  Processors.Day9{InputFilePath: getDayInputFile(9)},
+		10: Processors.Day10{InputFilePath: getDayInputFile(10)},
 	}
 }
 
